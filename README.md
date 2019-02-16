b2105794cf71a33ea7192f38c9f2544106510221
Phạm Kiều Thanh <hhnnaahhtt@gmail.com>
2019-02-16 19:53:29 +0700

    use new model

--- README.md
+++ README.md
@@ -8,63 +8,25 @@ npm i --save tokenhelper
 
 # Usage
 
-## exmaple
+## example
+# Standalone
 ```js
-const th = require('@subiz/tokenhelper')
-let token = new th.Token()
+const th = require('tokenhelper')
+let helper = th.newHelper({tokenep: "https://subiz.com/oauth/"})
+
 // user login => got tokens
-token.restart()
-token.set({account_id,agent_id,session,access_token, refreshtoken})
+token.reset({account_id,agent_id,session,access_token, refreshtoken})
 
 // use the token
 
-var token = token.get()
-if (token.error) {
-	throw token.error
+var tk = await token.get()
+if (tk.error) {
+	throw tk.error
 }
 
 // ...
-apireq.setQuery({access_token: token.access_token}).send()
+apireq.setQuery({access_token: tk.access_token}).send()
 // ...
 
-// token expired
-let err = await token.refresh()
-if (err) throw err
-// retrive new token
-{accesstoken, refreshtoken} = token.get()
 
-```
-
-# Design
-## State machine
-### NORMAL
-This state indicates that worker is waiting for refresh() request, transition to REFRESHING right after receive refresh().
-### DEAD
-This state indicates that the worker failed to refresh the token, there is nothing that user can do other than call restart(), it reject any refresh() call.
-### REFRESHING
-worker is trying to refresh token by sending a request to api server. This is an unstable state, mean that worker should not stay here too long. If the call to api server is successed, worker will have an new pair access token and refresh token, it should transition to JUST_REFRESHED state. Otherwise, if the call failed, worker will transition to DEAD state.
-Note that the call may fail because of another worker (on other tabs) have refreshed the token before this worker. In this case, new tokens are already been presisted in local storage. Worker must transition to JUST_REFRESHED state.
-### JUST REFRESHED
-this state indicates that worker have just refresh the tokens. The tokens is new and 'hot'. Any refresh() call will return immediately with these new tokens. After 10s, the tokens are 'cold' and might expired. Worker should transitino to NORMAL state so user can replace them with new tokens.
-```
-                                         restart()
-                      +-------------------------------------------------+
-                      |                                                 |
-                      |                                                 |
-                +-----v----+           +--------------+            +----+---+
-                |          | refresh() |              |   failed   |        |   refresh()
-refresh()+------>  NORMAL  +----------->  REFRESHING  +------------>  DEAD  <-----+
-                |          |           |              |            |        |
-                +-----^----+           +-------+------+            +----^---+
-                      |                        |                        |
-                      |                        |success                 +
-                      |                        |                     restart()
-                      |               +--------v---------+
-                      |      pass 10s |                  |
-                      +---------------+  JUST_REFRESHED  |
-                                      |                  |
-                                      +--------^---------+
-                                               |
-                                               +
-                                            refresh()
 ```
--- index.js
+++ index.js
@@ -1,189 +1,110 @@
 var store = require('store')
 var gAjax = require('@subiz/ajax/index.js')
 
-function resolveReq (msg) {
-	return function (req) {
-		req.resolve(msg)
-	}
-}
-
-function loop4everInternal (f, r) {
-	return f(function (delay, err) {
-		return err ? r(err) : setTimeout(loop4everInternal, delay, f, r)
-	})
-}
-
-function loop4ever (f) {
-	return new Promise(function (resolve) {
-		return loop4everInternal(f, resolve)
-	})
-}
-
 function isAccountChange (a, b) {
 	return a && b && (a.account_id !== b.account_id || a.agent_id !== b.agent_id)
 }
 
-function getStore () {
-	return store.get('sbztokn') || {}
+// isGoodToken tells whether a token has not expired or not about to expired
+function isGoodToken (created, expireInSec) {
+	var end = new Date(created).getTime() / 1000
+	end += expireInSec
+	// make token expire 60s sooner than it time
+	// since time between client and server cannot perfectly synchronized
+	// this reduce the chance that we tell an already expired token (in server) is good
+	end = end - 60
+	var now = Date().getTime() / 1000
+	return end < now
 }
 
-function setStore (k, v) {
-	return store.set(k, v)
+// isFunction tells whether f is a javascript function
+function isFunction (f) {
+	return f && {}.toString.call(f) === '[object Function]'
 }
 
-function run (self, state, param) {
-	return loop4ever(function (resolve) {
-		var f = self[state]
-		if (typeof f !== 'function') {
-			resolve(undefined, "invalid state '" + state + "', param " + param)
-			return
-		}
-		f.bind(self)(function (nextstate, nextparam, delay) {
-			state = nextstate
-			param = nextparam
-			resolve(parseInt(delay) || 1)
-		}, param)
+// loop executes function func multiple times
+function loop (func) {
+	if (!isFunction(func)) return
+	var called = false
+	func(function () {
+		if (called) return // make sure only call 1
+		called = true
+
+		setTimeout(loop, 1000, func)
 	})
 }
 
-function makeQueue () {
-	var q = []
-
-	var f = function () {
-		return new Promise(function (resolve) {
-			q.push({ resolve: resolve })
-		})
-	}
-
-	f.resolve = function (msg) {
-		q.forEach(resolveReq(msg))
-	}
-
-	f.reset = function () {
-		q = []
-	}
-
-	f.len = function () {
-		return q.length
-	}
-
-	return f
-}
-
-function getStoreFromData (data) {
-	var store = getStore()
-	if (isAccountChange(data, store)) {
-		return { error: 'account_changed' }
-	}
-	if (!data) data = store
-	if (!data) return {}
-	if (!data.refresh_token && !store.refresh_token) {
-		return { error: 'uninitialized' }
-	}
-	return Object.assign({}, data, store)
-}
-
-function filterData (param) {
-	return {
-		account_id: param.account_id,
-		agent_id: param.agent_id,
-		access_token: param.access_token,
-		refresh_token: param.refresh_token,
-		session: param.session,
-	}
-}
-
-function Token (param) {
+function createHelper (param) {
 	param = param || {}
 	var me = this
-	this.api = (param.ajax || gAjax)
+	me.api = (param.ajax || gAjax)
 		.post(param.tokenep)
 		.setParser('json')
 		.contentTypeForm()
-	this.refresh = makeQueue()
-	this.restart = makeQueue()
+	me.term = 0
 
-	this.get = function () {
-		return getStoreFromData(me.data)
-	}
+	me.get = function () {
+		return new Promise(function (rs) {
+			var tk = store.get(STOREID)
+			me.data = me.data || tk
+			if (!me.data) return rs({ error: 'uninitialized' })
+			if (isAccountChange(me.data, tk)) return rs({ error: 'account_changed' })
 
-	this.set = function (param) {
-		me.data = filterData(param)
-		setStore('sbztokn', me.data)
-	}
+			if (!isGoodToken(me.data.created, me.data.expires_in)) {
+				return rs(Object.assign({}, me.data))
+			}
 
-	this.NORMAL = function (transition) {
-		if (me.refresh.len() > 0) transition('REFRESHING')
-		else transition('NORMAL', undefined, 100)
-	}
+			var retry = 5
+			var p = { refresh_token: tk.refresh_token, session: tk.session }
+			var term = me.term
+			loop(function (continueLoop) {
+				me.api.send(p).then(function (code, body, err) {
+					if (term !== me.term) {
+						// our term has outdated, return latest data of new term
+						me.get().then(rs)
+						return
+					}
 
-	this.JUST_REFRESHED = function (transition, param) {
-		me.refresh.resolve()
-		me.refresh.reset()
-		param = param || { now: 0, then: 0 }
-		param.then = param.then + 100 || 100
-		if (param.then - param.now > 5000) transition('NORMAL')
-		else transition('JUST_REFRESHED', param, 100)
-	}
+					if ((err || code > 499) && retry > 0) {
+						// should retry 5 time
+						retry--
+						continueLoop()
+						return
+					}
 
-	this.REFRESHING = function (transition, param) {
-		var tk = me.get()
-		if (tk.error) return transition('DEAD', tk.error)
-		me.api
-			.send({ refresh_token: tk.refresh_token, session: tk.session })
-			.then(function (ret) {
-				var gtk = getStore()
-				var now = new Date()
-				var out = pureRefresh(now, tk, gtk, ret[0], ret[1], ret[2], param)
-				if (out[0]) me.set(out[0])
-				transition(out[1], out[2], out[3])
+					if (err || code !== 200) {
+						rs({ error: err })
+						return
+					}
+
+					if (isAccountChange(tk, store.get(STOREID) || {})) {
+						rs({ error: 'account_changed' })
+						return
+					}
+
+					me.reset(body) // update internal token using returned body
+					rs(me.data)
+				})
 			})
+		})
 	}
 
-	this.DEAD = function (transition, msg) {
-		me.refresh.resolve('dead ' + msg)
-		me.refresh.reset()
-
-		if (!me.restart.len()) return transition('DEAD', undefined, 100)
-		me.restart.resolve()
-		me.restart.reset()
-		transition('NORMAL')
-	}
-
-	if (!param.dry) run(this, 'NORMAL')
-}
-
-function pureRefresh (now, ltk, gtk, code, body, err, param) {
-	if (err || code > 499) {
-		// network error or server error
-		var retry = (param && param.retry) || 0
-		if (retry > 5) return [undefined, 'DEAD', 'server_down']
-		return [ltk, 'REFRESHING', { retry: retry + 1, err: err, body: body }, 1000]
-	}
-	if (code !== 200) {
-		if (isAccountChange(ltk, gtk)) {
-			return [undefined, 'DEAD', 'account_changed']
+	me.reset = function (body) {
+		me.term++
+		me.data = {
+			account_id: body.account_id,
+			agent_id: body.agent_id,
+			access_token: body.access_token,
+			refresh_token: body.refresh_token,
+			session: body.session,
+			created: new Date(),
+			expires_in: body.expires_in, // 3600
 		}
-		// someone have exchanged the token
-		if (gtk.refresh_token && gtk.refresh_token !== ltk.refresh_token) {
-			return [gtk, 'JUST_REFRESHED', { now: now }]
-		}
-		return [undefined, 'DEAD', 'expired']
-	}
 
-	var newtok = {
-		access_token: body.access_token,
-		refresh_token: body.refresh_token,
-		account_id: body.account_id,
-		id: body.id,
-		session: body.session,
+		store.set(STOREID, me.data)
 	}
-	return [newtok, 'JUST_REFRESHED', { now: now }]
 }
 
-module.exports = {
-	Token: Token,
-	loop4ever: loop4ever,
-	run: run,
-	pureRefresh: pureRefresh,
-}
+module.exports = { createHelper: createHelper }
+
+var STOREID = 'sbztokn'
